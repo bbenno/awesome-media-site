34bc9ae71c9e42c137342dbbe18ea75635169a73
Benno Bielmeier <github@bbenno.com>
2021-07-29 20:10:22 +0200

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+source 'https://rubygems.org'
+
+git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
+
+gem 'middleman', '~> 4.4.0'
+## Indicate a current (active) link using `aria-current`
+gem 'middleman-aria_current', '~> 0.1.2'
+## Vendor-prefix stylesheets
+gem 'middleman-autoprefixer', '~> 3.0.0'
+# gem 'middleman-imageoptim', '~> 0.3.0'
+## Inlining SVGs in views
+# gem 'middleman-inline_svg'
+## Refresh page in browser on file edits
+gem 'middleman-livereload', '~> 3.4.7'
+## Minify whitespaces around HTML
+gem 'middleman-minify-html', '~> 3.4.1'
+gem 'middleman-robots', '~> 1.3.5'
+gem 'middleman-sitemap', '~> 0.0.13', github: '/matsu911/middleman-sitemap'
+
+## Templating engine
+gem 'slim', '~> 4.1'
+
+gem 'tzinfo-data', platforms: %i[mswin mingw jruby x64_mingw]
+gem 'wdm', '~> 0.1', platforms: %i[mswin mingw x64_mingw]
+
+gem 'solargraph', '~> 0.43.0', group: :development
---
+++ config.rb
@@ -0,0 +1,77 @@
+## Activate and configure extensions
+
+activate :autoprefixer do |prefix|
+  prefix.browsers = "last 2 versions"
+end
+
+activate :i18n, mount_at_root: :en
+
+## Generates Web Robots Page
+## http://www.robotstxt.org/
+activate :robots,
+         rules: [{ user_agent: '*', allow: ['/'] }],
+         sitemap: '/sitemap.xml'
+
+acitvate :sitemap,
+         gzip: true
+         hostname: 'https://benno.github.io/middleman-template'
+
+##
+
+set :css_dir, 'assets/stylesheets'
+set :js_dir, 'assets/javascripts'
+set :images_dir, 'assets/images'
+set :fonts_dir, 'assets/fonts'
+
+## Layouts
+
+## Per-page layout changes
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
+
+## With alternative layout
+# page '/path/to/file.html', layout: 'other_layout'
+
+## Proxy pages
+
+# proxy(
+#   '/this-page-has-no-template.html',
+#   '/template-file.html',
+#   locals: {
+#     which_fake_page: 'Rendering a fake page with a local variable'
+#   },
+# )
+
+## Helpers
+## Methods defined in the helpers block are available in templates
+
+# helpers do
+#   def some_helper
+#     'Helping'
+#   end
+# end
+
+## Build-specific configuration
+
+configure :build do
+  # Use relative URLs
+  activate :directory_indexes
+
+  # Add assets fingerprinting to avoid cache issues
+  activate :asset_hash
+end
+
+configure :development do
+  activate :livereload
+  set :debug_assets, true
+
+  ::Slim::Engine.set_options pretty: true
+end
+
+configure :production do
+  activate :gzip
+  activate :minify_css
+  activate :minify_html
+  activate :minify_javascript
+end
