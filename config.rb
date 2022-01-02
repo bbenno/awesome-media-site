d4ae5e38a214b644d920ce1ca168a881d3039619
Benno Bielmeier <github@bbenno.com>
2022-01-02 21:34:44 +0100

    Fix external pipeline
    Accidentally deleted in d7b1f441b925

--- config.rb
+++ config.rb
@@ -51,13 +51,9 @@ page '/*.txt', layout: false
 ###############################################################################
 ## Proxy pages
 
-# proxy(
-#   '/this-page-has-no-template.html',
-#   '/template-file.html',
-#   locals: {
-#     which_fake_page: 'Rendering a fake page with a local variable'
-#   },
-# )
+activate :external_pipeline, name: :download_images,
+                             command: './lib/download_images.rb',
+                             source: 'assets/images'
 
 ###############################################################################
 ## Helpers
