9634a6f6043f24e34c5710ae5628a3b484ef6bff
Benno Bielmeier <github@bbenno.com>
2022-02-14 21:31:06 +0100

    Fix image download

--- lib/download_images.rb
+++ lib/download_images.rb
@@ -26,7 +26,7 @@ data.each do |media_type, media_items|
       next
     end
 
-    path = File.join('source', 'assets', 'images', "#{media['tmdb']}")
+    path = File.join('source', 'assets', 'images', media['tmdb'].to_s)
 
     if Dir.glob("#{path}.*").any?
       logger.info("Skip #{media['title']} (#{media['tmdb']})")
@@ -35,7 +35,8 @@ data.each do |media_type, media_items|
       logger.info("Loading #{media['title']} (#{media['tmdb']})")
     end
 
-    uri = URI("#{tmdb_api_url}/#{media_type}/#{media['tmdb']}/images?api_key=#{tmdb_api_key}")
+    #uri = URI("#{tmdb_api_url}/#{media_type}/#{media['tmdb']}/images?api_key=#{tmdb_api_key}")
+    uri = URI("#{tmdb_api_url}/#{media_type}/#{media['tmdb']}?api_key=#{tmdb_api_key}&language=#{I18n.locale}")
     res = Net::HTTP.get_response(uri)
 
     unless res.is_a?(Net::HTTPSuccess)
@@ -43,7 +44,8 @@ data.each do |media_type, media_items|
       next
     end
 
-    file_path = JSON.parse(res.body)['posters'][0]['file_path']
+    #file_path = JSON.parse(res.body)['posters'][0]['file_path']
+    file_path = JSON.parse(res.body)['poster_path']
 
     image_uri = URI("#{tmdb_image_base_url}#{tmdb_image_file_size}#{file_path}")
     ext = File.extname(file_path)
