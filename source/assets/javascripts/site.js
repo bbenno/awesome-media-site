07511bab917bbbbc71c0f763928f895b924e3569
Benno Bielmeier <github@bbenno.com>
2022-02-14 17:55:27 +0100

    Allow user to mark media as watched and safe locally

--- source/assets/javascripts/site.js
+++ source/assets/javascripts/site.js
@@ -1,2 +1,18 @@
 //= require popper
 //= require bootstrap
+
+function updateStorage(target) {
+	var previous = localStorage.getItem(target.id);
+	var next = previous ^ true;
+  localStorage.setItem(target.id, next);
+
+	if (next == 1) target.style.opacity = .4;
+	else target.style.opacity = 1.0;
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+	document.querySelectorAll('.card').forEach(cardElem => {
+		if (localStorage.getItem(cardElem.id) == 1) cardElem.style.opacity = .4;
+		cardElem.addEventListener('click', () => updateStorage(cardElem));
+	});
+});
