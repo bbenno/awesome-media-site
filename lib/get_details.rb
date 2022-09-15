1e6d98ba61e5249108e6c06a8f33852e2e2203ec
Benno Bielmeier <github@bbenno.com>
2022-09-15 19:19:53 +0200

    Refactor lib/get_details.rb

--- lib/get_details.rb
+++ lib/get_details.rb
@@ -9,22 +9,26 @@ require 'net/http'
 def api_key = ENV.fetch('TMDB_API_KEY')
 def dir_path = File.join('data', 'details')
 
+Category = Struct.new(:name, :tmdb_key) do
+  def content = YAML.load(File.open("data/#{name}.yml"))
+end
+
 Dir.mkdir(dir_path) unless Dir.exist?(dir_path)
 
-{
-  movie: YAML.load(File.open('data/movies.yml')).concat(YAML.load(File.open('data/short_films.yml'))),
-  tv: YAML.load(File.open('data/series.yml'))
-}.each do |media_type, media_items|
-  media_items.each do |media|
-    abort unless media['tmdb']
+[ Category.new('movies', 'movie'),
+  Category.new('short_films', 'movie'),
+  Category.new('series', 'tv')
+].each do |category|
+  details_path = File.join(dir_path, category.name)
+  Dir.mkdir(details_path) unless Dir.exist?(details_path)
+  category.content.each do |media|
+    abort unless (id = media['tmdb'])
 
-    id = media['tmdb']
-
-    file_path = File.join(dir_path, "#{id}.json")
+    file_path = File.join(details_path, "#{id}.json")
 
     next if File.exist?(file_path)
 
-    uri = URI("https://api.themoviedb.org/3/#{media_type}/#{id}?api_key=#{api_key}")
+    uri = URI("https://api.themoviedb.org/3/#{category.tmdb_key}/#{id}?api_key=#{api_key}")
     res = Net::HTTP.get_response(uri)
 
     abort("Failed with #{res.class}") unless res.is_a?(Net::HTTPSuccess)
@@ -33,7 +37,9 @@ Dir.mkdir(dir_path) unless Dir.exist?(dir_path)
 
     JSON.parse!(res.body).then do |m|
       img = URI('https://image.tmdb.org/t/p/w342').tap { _1.path += m['poster_path'] }
-      File.write(File.join('source', 'assets', 'images', (m['title'] || m['name']).parameterize), Net::HTTP.get(img))
+      path = File.join('source', 'assets', 'images', category.name)
+      Dir.mkdir(path) unless Dir.exist?(path)
+      File.write(File.join(path, (m['title'] || m['name']).parameterize), Net::HTTP.get(img))
     end
   end
 end
