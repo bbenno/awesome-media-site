4ea46d2bbee47a540928cce79fe183b5bef75596
Benno Bielmeier <git@bbenno.com>
2025-04-23 09:28:34 +0200

    Update image / details downloader script

--- lib/get_details.rb
+++ lib/get_details.rb
@@ -19,10 +19,13 @@ Dir.mkdir(dir_path) unless Dir.exist?(dir_path)
   Category.new('short_films', 'movie'),
   Category.new('series', 'tv')
 ].each do |category|
+  puts "CATEGORY: #{category.name}"
+
   details_path = File.join(dir_path, category.name)
   Dir.mkdir(details_path) unless Dir.exist?(details_path)
-  category.content.each do |media|
+  category.content.shuffle.each do |media|
     abort("missing tmdb id for #{media}") unless (id = media['tmdb'])
+    puts "MEDIA: #{media['title']}"
 
     file_path = File.join(details_path, "#{id}.json")
 
@@ -39,7 +42,10 @@ Dir.mkdir(dir_path) unless Dir.exist?(dir_path)
       img = URI('https://image.tmdb.org/t/p/w342').tap { _1.path += m['poster_path'] }
       path = File.join('source', 'assets', 'images', category.name)
       Dir.mkdir(path) unless Dir.exist?(path)
-      File.write(File.join(path, (m['title'] || m['name']).parameterize), Net::HTTP.get(img))
+      year = m['release_date']&.slice(0, 4) || m['first_air_date']&.slice(0, 4) || media['year']
+      title = m['title'] || media ['title']
+      File.write(File.join(path, [title, year].compact.join('+').gsub!(/[^a-z0-9\-_]+/i, '-')), Net::HTTP.get(img))
     end
+    sleep 0.1 # Rate Limit
   end
 end
